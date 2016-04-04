fd29e17d7c64af145d872606238affaf70c0d4be
josemi05@gmail.com <Alumno@10.0.2.15>
2016-04-04 13:28:30 +0200

    Primera operaciones

--- Calculadora/src/Calculadora.java
+++ Calculadora/src/Calculadora.java
@@ -1,4 +1,21 @@
 
 public class Calculadora {
-
+	int numero;
+	int numero2;
+	public Calculadora(int n1, int n2){
+		numero=n1;
+		numero2=n2;
+	}
+	public int suma(){
+		return numero+numero2;
+	}
+	public int resta(){
+		return numero-numero2;
+	}
+	public int multiplicar(){
+		return numero*numero2;
+	}
+	public double division(){
+		return (double)this.numero/this.numero2;
+	}
 }
